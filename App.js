ee9db7216f7ea7e5cf92067464677d464f31534c
RossDee <diying@sjtu.edu.cn>
2023-10-15 23:19:20 +0800

    Update upcoming weather component

--- App.js
+++ App.js
@@ -1,24 +1,19 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
-import CurrentWeather from "./src/components/CurrentWeather";
-import UpcomingWeather from "./src/components/UpcomingWeather";
+import UpcomingWeather from "./src/screens/UpcomingWeather";
+import City from "./src/screens/City";
 
 const App = () => {
   return (
     <View style={styles.container}>
-      <UpcomingWeather />
+      <City />
     </View>
   );
-} 
+};
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "pink",
-    alignItems: "center",
-    justifyContent: "center",
   },
 });
-
-
 
 export default App;
